7c9125f5d8144fe5ef9d34b4497b306650f6f016
Araya <adyranggahidayat@gmail.com>
2022-05-11 19:40:12 +0700

    add unit tests

--- araya_core_screen/test/araya_core_screen_test.dart
+++ araya_core_screen/test/araya_core_screen_test.dart
@@ -1,5 +1,151 @@
+import 'package:araya_core_screen/araya_core_screen.dart';
+import 'package:araya_core_screen/araya_overlay.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+class FakeScreen extends StatefulWidget {
+  const FakeScreen({Key? key, required this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  State<FakeScreen> createState() => _FakeScreenState();
+}
+
+class _FakeScreenState extends State<FakeScreen> with ArayaCoreScreenImpl {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    super.setApplicationSwitcherDescription(context, 'testWidget');
+    ScreenSize screenSize = super.buildScreenSize(context);
+    return Scaffold(
+      key: _scaffoldKey,
+      endDrawer: widget.child,
+      body: SizedBox(
+        width: screenSize.isMobile() ? screenSize.width : screenSize.height,
+        height: screenSize.height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ElevatedButton(
+                onPressed: () => _scaffoldKey.currentState?.openEndDrawer(),
+                child: const Text('Open EndDrawer')),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  ArayaSnackBar.loadingOverlay(
+                    context,
+                    duration: const Duration(milliseconds: 500),
+                  ),
+                );
+              },
+              child: const Text('Show Snackbar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget _buildTestableWidget(Widget widget, [Size size = const Size(480, 700)]) {
+  return MediaQuery(
+    data: MediaQueryData(size: size),
+    child: MaterialApp(home: widget),
+  );
+}
+
 void main() {
-  test('add test here', () {});
+  group('Araya Core Screen - ArayaOverlay class', () {
+    testWidgets('EndDrawer as Overlay must has a spinner',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+          _buildTestableWidget(const FakeScreen(child: ArayaOverlayLoading())));
+      await tester.pump();
+      ElevatedButton button = find
+          .widgetWithText(ElevatedButton, 'Open EndDrawer')
+          .evaluate()
+          .first
+          .widget as ElevatedButton;
+      button.onPressed!();
+      await tester.pump();
+      final spinner = find.byKey(const Key('spinner'));
+      expect(spinner, findsOneWidget);
+    });
+    testWidgets('EndDrawer as Overlay spinner dismissable',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        _buildTestableWidget(const FakeScreen(
+          child: ArayaOverlayLoading(
+            key: Key('arayaOverlayLoading'),
+            loadingDismissible: true,
+          ),
+        )),
+      );
+      await tester.pump();
+      ElevatedButton button = find
+          .widgetWithText(ElevatedButton, 'Open EndDrawer')
+          .evaluate()
+          .first
+          .widget as ElevatedButton;
+      button.onPressed!();
+      await tester.pump();
+      final spinner = find.byKey(const Key('spinner'));
+      expect(spinner, findsOneWidget);
+      GestureDetector detector = find
+          .byKey(const Key('spinnerTapDetector'))
+          .evaluate()
+          .first
+          .widget as GestureDetector;
+      detector.onTap!();
+      await tester.pumpAndSettle();
+      expect(find.byKey(const Key('spinner')), findsNothing);
+    });
+    testWidgets('EndDrawer as Overlay with Custom Contents',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(_buildTestableWidget(
+        FakeScreen(
+          child: ArayaOverlay(
+            barrierDismissible: true,
+            childBuilder: (context) => const Center(
+              child: Text('Overlay Content'),
+            ),
+          ),
+        ),
+      ));
+      await tester.pump();
+      ElevatedButton button = find
+          .widgetWithText(ElevatedButton, 'Open EndDrawer')
+          .evaluate()
+          .first
+          .widget as ElevatedButton;
+      button.onPressed!();
+      await tester.pump();
+      final spinner = find.text('Overlay Content');
+      expect(spinner, findsOneWidget);
+      GestureDetector barrier = find
+          .byKey(const Key('arayaOverlayBarrier'))
+          .evaluate()
+          .first
+          .widget as GestureDetector;
+      barrier.onTap!();
+      await tester.pumpAndSettle();
+      expect(find.byKey(const Key('arayaOverlayBarrier')), findsNothing);
+    });
+    testWidgets('SnackBar as Overlay must has a spinner',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+          _buildTestableWidget(const FakeScreen(child: ArayaOverlayLoading())));
+      await tester.pump();
+      ElevatedButton button = find
+          .widgetWithText(ElevatedButton, 'Show Snackbar')
+          .evaluate()
+          .first
+          .widget as ElevatedButton;
+      button.onPressed!();
+      await tester.pump(const Duration(seconds: 1));
+      final spinner = find.byKey(const Key('spinner'));
+      expect(spinner, findsOneWidget);
+    });
+  });
 }
--- araya_core_utils/test/araya_core_utils_test.dart
+++ araya_core_utils/test/araya_core_utils_test.dart
@@ -1,5 +1,56 @@
+import 'package:araya_core_utils/araya_core_utils.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('add test here', () {});
+  group('Araya Core Utils - fromHex method', () {
+    test('8 digits hex color, color must be white', () {
+      Color color = ArayaCoreUtils.fromHex('#FFFFFFFF');
+      expect(color, Colors.white);
+    });
+    test('6 digits hex color, color must be white', () {
+      Color color = ArayaCoreUtils.fromHex('#FFFFFF');
+      expect(color, Colors.white);
+    });
+  });
+
+  group('Araya Core Utils - currencyFormat method', () {
+    test('value must be Rp.0', () {
+      String value = ArayaCoreUtils.currencyFormat(0);
+      expect(value, 'Rp.0');
+    });
+    test('value must be Rp.0', () {
+      String value = ArayaCoreUtils.currencyFormat(0.0);
+      expect(value, 'Rp.0');
+    });
+    test('value must be Rp.1.000', () {
+      String value = ArayaCoreUtils.currencyFormat(1000,
+          locale: 'en-US', symbol: '\$ ', decimalDigit: 2);
+      expect(value, '\$ 1,000.00');
+    });
+  });
+
+  group('Araya Core Utils - unformatCurrency method', () {
+    test('value must be 0', () {
+      String value = ArayaCoreUtils.unformatCurrency('');
+      expect(value, '0');
+    });
+    test('value must be empty', () {
+      String value = ArayaCoreUtils.unformatCurrency('', defaultValue: '');
+      expect(value, '');
+    });
+    test('value must be 0', () {
+      String value = ArayaCoreUtils.unformatCurrency('0.1');
+      expect(value, '01');
+    });
+    test('value must be 1000', () {
+      String value = ArayaCoreUtils.unformatCurrency('Rp.1000');
+      expect(value, '1000');
+    });
+    test('value must be 100', () {
+      String value =
+          ArayaCoreUtils.unformatCurrency('RP.1.00', decimalSign: '.');
+      expect(value, '1.00');
+    });
+  });
 }
